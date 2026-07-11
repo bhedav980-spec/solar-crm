fa91823d1d69c2bfb39d31ac49c1e60377acfcd7
bhedav980-spec <bhedav980@gmail.com>
2026-07-11 17:02:44 +0530

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://bsvxqhxyexhbgysnfgal.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_yg-F8bDfTJEZPAHr38TwJw_Yue23cpg";
-
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
